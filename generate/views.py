f509a59fb12243cc8f7f06df8b9c79b5d1e7635c
Plamen Stoilkov <sdplamen@gmail.com>
2025-09-15 10:34:33 +0300

    Update API views

--- generate/views.py
+++ generate/views.py
@@ -9,16 +9,61 @@ from generate.serializers import GradientSerializer
 
 
 # Create your views here.
-def generate_gradient(request): 
+# def _prepare_context(request, colors) :
+#     form = GradientForm(initial={f'color{i + 1}' :colors[i] for i in range(len(colors))})
+#
+#     gradient = f'linear-gradient(to top, {", ".join(colors)})'
+#     css_code = f'background: {gradient};'
+#
+#     saved_palettes = ColorPalette.objects.all().order_by('created_at')
+#     paginator = Paginator(saved_palettes, 10)
+#     page_number = request.GET.get('page')
+#     page_obj = paginator.get_page(page_number)
+#
+#     return {
+#         'form' :form,
+#         'css_code' :css_code,
+#         'colors' :colors,
+#         'saved_palettes' :page_obj,
+#     }
+#
+# def generate_gradient(request) :
+#     colors = []
+#
+#     if request.method == 'POST' :
+#         form = GradientForm(request.POST)
+#         if form.is_valid() :
+#             colors = [form.cleaned_data[f'color{i + 1}'] for i in range(6)]
+#             ColorPalette.objects.create(colors=colors)
+#             return redirect(f'/gradient/?{request.POST.urlencode()}')
+#
+#     form = GradientForm(request.GET or None)
+#     if form.is_valid() :
+#         colors = [form.cleaned_data[f'color{i + 1}'] for i in range(6)]
+#     else :
+#         colors = get_gradient_colors()
+#         form = GradientForm(initial={f'color{i + 1}' :colors[i] for i in range(6)})
+#
+#     context = _prepare_context(request, colors)
+#     context['form'] = form
+#     return render(request, 'index.html', context)
+#
+# def get_palette(request, palette_id):
+#     palette = get_object_or_404(ColorPalette, id=palette_id)
+#     colors = palette.colors
+#     context = _prepare_context(request, colors)
+#     return render(request, 'index.html', context)
+
+def generate_gradient(request):
     colors = []
 
-    if request.method == 'GET': 
-        if 'random' in request.GET and request.GET['random'] == 'true': 
+    if request.method == 'GET':
+        if 'random' in request.GET and request.GET['random'] == 'true':
             colors = get_gradient_colors()
             form = GradientForm(initial={
                 f'color{i + 1}': colors[i] for i in range(6)
             })
-        else: 
+        else:
             form = GradientForm(request.GET or None)
             if form.is_valid():
                 colors = [form.cleaned_data[f'color{i + 1}'] for i in range(6)]
@@ -72,37 +117,78 @@ def get_palette(request, palette_id):
     return render(request, 'index.html', context)
 
 class GradientAPIView(APIView):
-    def get(self, request, palette_id=None):
-        if palette_id: 
-            palette = get_object_or_404(ColorPalette, id=palette_id)
-            colors = palette.colors
+    def get(self, request):
+        colors = []
+        if request.query_params.get('random') == 'true':
+            colors = get_gradient_colors()
+        else:
+            form = GradientForm(request.query_params or None)
+            if form.is_valid():
+                colors = [form.cleaned_data[f'color{i + 1}'] for i in range(6)]
+            else:
+                colors = get_gradient_colors()
 
-            data = {
-                'colors': colors,
-                'gradient': f'linear-gradient(to top, {", ".join(colors)})',
-                'css_code': f'background: linear-gradient(to top,, {", ".join(colors)});'
-            }
-            serializer = GradientSerializer(data=data)
-            if serializer.is_valid(): 
-                return Response(serializer.data)
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        else: 
-            palettes = ColorPalette.objects.all().order_by('-created_at')
-            serialized_palettes = []
-            for palette in palettes:
-                data = {
-                    'id': palette.id,
-                    'colors': palette.colors,
-                    'gradient': f'linear-gradient(to top, {", ".join(palette.colors)})',
-                    'css_code': f'background: linear-gradient(to top, {", ".join(palette.colors)});'
-                }
-                serialized_palettes.append(data)
-            return Response(serialized_palettes)
+        gradient = f'linear-gradient(to top, {", ".join(colors)})'
+        css_code = f'background: {gradient};'
 
-    def post(self, request, *args, **kwargs): 
-        serializer = GradientSerializer(data=request.data)
-        if serializer.is_valid(): 
-            colors = serializer.validated_data['colors']
-            ColorPalette.objects.create(colors=colors)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        saved_palettes = ColorPalette.objects.all().order_by('created_at')
+        paginator = Paginator(saved_palettes, 10)
+        page_number = request.query_params.get('page')
+        page_obj = paginator.get_page(page_number)
+
+        data = {
+            'direction': 'to top',
+            'colors': colors,
+            'gradient': gradient,
+            'css_code': css_code,
+        }
+        serializer = GradientSerializer(data)
+        response_data = serializer.data
+        response_data['saved_palettes'] = [
+            {'id': palette.id, 'colors': palette.colors} for palette in page_obj
+        ]
+        return Response(response_data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        form = GradientForm(request.data)
+        if not form.is_valid():
+            return Response(form.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        colors = [form.cleaned_data[f'color{i + 1}'] for i in range(6)]
+        gradient = f'linear-gradient(to top, {", ".join(colors)})'
+        css_code = f'background: {gradient};'
+        ColorPalette.objects.create(colors=colors)
+
+        data = {
+            'direction': 'to top',
+            'colors': colors,
+            'gradient': gradient,
+            'css_code': css_code,
+        }
+        serializer = GradientSerializer(data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+class PaletteAPIView(APIView):
+    def get(self, request, palette_id):
+        palette = get_object_or_404(ColorPalette, id=palette_id)
+        colors = palette.colors
+        gradient = f'linear-gradient(to top, {", ".join(colors)})'
+        css_code = f'background: {gradient};'
+
+        saved_palettes = ColorPalette.objects.all().order_by('created_at')
+        paginator = Paginator(saved_palettes, 10)
+        page_number = request.query_params.get('page')
+        page_obj = paginator.get_page(page_number)
+
+        data = {
+            'direction': 'to top',
+            'colors': colors,
+            'gradient': gradient,
+            'css_code': css_code,
+        }
+        serializer = GradientSerializer(data)
+        response_data = serializer.data
+        response_data['saved_palettes'] = [
+            {'id': palette.id, 'colors': palette.colors} for palette in page_obj
+        ]
+        return Response(response_data, status=status.HTTP_200_OK)
